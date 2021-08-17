4f7c13f422e41403ce6953db405357866ac0751b
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-08-16 20:10:13 -0400

    adds Main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
-     map challenge
+     <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => {
+	return <div>Map Challenge</div>;
+};
+
+export default Main;
