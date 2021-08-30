687a2bbf21217e28259ff69bf6a8cad13bdbeeff
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-08-30 15:36:10 -0400

    adds simple map library and state

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import MapChart from './components/Mapchart';
 //map in here
 const Main = () => {
-	return <div>Map Challenge</div>;
+	const [selectCountry, setSelectCountry] = useState('');
+	const [countryToMatch, setCountryToMatch] = useState('France');
+
+	return (
+		<div>
+			<h2>Find {countryToMatch} on the map!</h2>
+			<MapChart setSelectCountry={setSelectCountry} />
+			<p>{selectCountry === countryToMatch ? 'Correct!' : 'Wrong!'}</p>
+		</div>
+	);
 };
 
 export default Main;
